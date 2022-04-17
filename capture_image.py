02fa5abba6704086bccc83efdcadfe9ebea32853
ismeterd <erdoganis20@itu.edu.tr>
2022-04-17 17:13:53 +0300

    Program is written again with OOP.

--- capture_image.py
+++ capture_image.py
@@ -1,56 +1,37 @@
 # importing OpenCV library
 import cv2
 
-# initialize the camera
-# it can be multiple camera connected with pc
-# you can change the cam_port to select another camera
 
-cam_port = 1  # my webcam port
-cam = cv2.VideoCapture(cam_port)
+class CaptureImage():
 
-def run():
-    valid = input("Enter the 'ok' to capture a image: ")
+    def __init__(self, cam_port):
+        self.cam_port = cam_port
+        self.cam = cv2.VideoCapture(self.cam_port)
+        self.result, self.image = self.cam.read()
 
-    if (valid == "ok"):
-        result, image = cam.read()
-        print("Capturing the image...")
-    else:
-        print("quitting the program...")
-        return False
+    def capture(self):
+        self.result, self.image = self.cam.read()
 
-    valid = input("Enter the 'show' to show capturing image: ")
-
-    if (valid == "show" and result == True):
-        print(result)
-        print(image)
-        cv2.imshow("CapturedImage", image)
+    def show(self):
+        cv2.imshow("CapturedImage", self.image)
         cv2.waitKey()
-    else:
-        print("quitting the program...")
-        return False
-
-    valid = input("Enter the 'save' to save capturing image: ")
-
-    if (valid == "save"):
-        cv2.imwrite("images/captured_image.png", image)
-
-    else:
-        print("quitting the program...")
-        return False
-
-    valid = input("Do you wanna capture a image again?(yes): ")
-
-    if (valid == "yes"):
-        print("Starting again...")
-
-    else:
         cv2.destroyWindow("CapturedImage")
-        print("quitting the program...")
-        return False
+
+    def save(self):
+        cv2.imwrite("images/captured_image.png", self.image)
+
 
 if __name__ == "__main__":
-    while(True):
-        program = run()
-        if program == False:
-            break
 
+    image = CaptureImage(1)  # cam_port is selected 1 for my webcam
+
+    while (True):
+        valid = input("Do you want to capture and save image(yes): ")
+
+        if valid == "yes":
+            image.capture()
+            image.save()
+
+        else:
+            print("quitting the program...")
+            break
